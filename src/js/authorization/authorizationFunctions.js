7c11360862414661fd4d0219441bd8e7f4397a9f
Yurii Soroka <yurisoroka165@gmail.com>
2023-04-13 17:33:08 +0300

    some work

---
+++ src/js/authorization/authorizationFunctions.js
@@ -0,0 +1,113 @@
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "./firebaseConfig";
+
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+} from "firebase/auth";
+import { getDatabase, set, update, ref, push } from "firebase/database";
+import Notiflix from "notiflix";
+
+import { modalToggle, clearEventListeners } from "../modal-window";
+
+// ініціалізація додатку, авторизації, та бази даних firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const database = getDatabase(app);
+
+const currentDate = new Date();
+
+// функція реєстрації
+export function registerSubmitHandler(event) {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const login = form.elements.login.value;
+    const password = form.elements.password.value;
+
+    createUserWithEmailAndPassword(auth, login, password)
+        .then(userCredential => {
+            const user = userCredential.user;
+
+            set(ref(database, "users/" + user.uid), {
+                email: login,
+                registerDate: currentDate.toLocaleString(),
+                queue: [],
+                watched: [],
+            })
+                .then(() => {
+                    Notiflix.Notify.success("Реєстрація успішна");
+                    form.reset();
+                    modalToggle();
+                    clearEventListeners();
+                })
+                .catch(error => {
+                    Notiflix.Notify.failure(
+                        `Помилка реєстрації ${error.message}`
+                    );
+                });
+        })
+        .catch(error => {
+            const errorMessage = error.message;
+            Notiflix.Notify.failure(`Помилка реєстрації ${errorMessage}`);
+        });
+}
+
+// функція логіну
+export function loginSubmitHandler(event) {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const login = form.elements.login.value;
+    const password = form.elements.password.value;
+
+    signInWithEmailAndPassword(auth, login, password)
+        .then(userCredential => {
+            const user = userCredential.user;
+            // додати або оновити властивість lastLoginDate в конкретного користувача
+            update(ref(database, "users/" + user.uid), {
+                lastLoginDate: currentDate.toLocaleString(),
+            })
+                .then(() => {
+                    Notiflix.Notify.success("Авторизація успішна");
+                    form.reset();
+                    modalToggle();
+                    clearEventListeners();
+                })
+                .catch(error => {
+                    Notiflix.Notify.failure(`Помилка входу ${error.message}`);
+                });
+        })
+        .catch(error => {
+            const errorMessage = error.message;
+            Notiflix.Notify.failure(`Помилка входу ${errorMessage}`);
+        });
+}
+
+export function logOff() {
+    signOut(auth)
+        .then(() => {
+            Notiflix.Notify.warning("Авторизацію скасовано");
+        })
+        .catch(error => {
+            Notiflix.Notify.warning(
+                `Виникли проблеми при виході: ${error.message}`
+            );
+        });
+}
+
+export function writeUserDataToDatabase(data, type) {
+    const userId = auth.currentUser.uid;
+    update(ref(database, "users/" + userId), {
+        queue: data,
+    });
+}
+
+// export function writeUserDataToDatabase(data) {
+//     const userId = auth.currentUser.uid;
+//     const postListRef = ref(database, "users/" + userId);
+//     const newPostRef = push(postListRef);
+//     set(newPostRef, {
+//         queue: data
+//     })
+// }
--- src/js/modal-window.js
+++ src/js/modal-window.js
@@ -1,31 +1,13 @@
-import { initializeApp } from "firebase/app";
+import { nanoid } from "nanoid";
+
 import {
-    getAuth,
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    signOut,
-} from "firebase/auth";
-import { getDatabase, set, update, ref } from "firebase/database";
-import Notiflix from "notiflix";
+    registerSubmitHandler,
+    loginSubmitHandler,
+    logOff,
+    writeUserDataToDatabase,
+} from "./authorization/authorizationFunctions";
 
-// дані firebase
-const firebaseConfig = {
-    apiKey: "AIzaSyCRTPr2q4rbgHs0n3SJxofCEvC1QedTgPQ",
-    authDomain: "filmoteka-ad98a.firebaseapp.com",
-    databaseURL:
-        "https://filmoteka-ad98a-default-rtdb.europe-west1.firebasedatabase.app/",
-    projectId: "filmoteka-ad98a",
-    storageBucket: "filmoteka-ad98a.appspot.com",
-    messagingSenderId: "199206793785",
-    appId: "1:199206793785:web:6e36302426a4d27ab093e8",
-};
-
-const lastLoginDate = new Date();
-
-// ініціалізація додатку, авторизації, та бази даних firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth();
-const database = getDatabase(app);
+const rawData = [{ id: nanoid(), name: "Yurii Soroka", number: "111-11-11" }];
 
 const refs = {
     loginBtn: document.querySelector(".login-button"),
@@ -38,20 +20,23 @@ const refs = {
     writeBtn: document.querySelector(".write-button"),
 };
 
-refs.loginBtn.addEventListener("click", () => toggleModal("login"));
-refs.signinBtn.addEventListener("click", () => toggleModal("signin"));
+refs.loginBtn.addEventListener("click", () =>
+    authorizationFormChanger("login")
+);
+refs.signinBtn.addEventListener("click", () =>
+    authorizationFormChanger("signin")
+);
 refs.closeModalBtn.addEventListener("click", onCloseModal);
 refs.logOffBtn.addEventListener("click", logOff);
 refs.writeBtn.addEventListener("click", () => writeUserDataToDatabase(rawData));
 
 function onCloseModal() {
-    refs.modal.classList.toggle("modal-hidden");
-    refs.modalForm.removeEventListener("submit", registerSubmitHandler);
-    refs.modalForm.removeEventListener("submit", loginSubmitHandler);
+    modalToggle();
+    clearEventListeners();
 }
 
-// функція показу приховування модального вікна входу
-function toggleModal(trigger) {
+// функція змінює імя кнопки сабиіту та ставить слухач з відповідною функцією
+function authorizationFormChanger(trigger) {
     if (trigger === "signin") {
         refs.modalForm.addEventListener("submit", registerSubmitHandler);
         refs.modalSubmitBtn.textContent = "Register";
@@ -59,104 +44,14 @@ function toggleModal(trigger) {
         refs.modalForm.addEventListener("submit", loginSubmitHandler);
         refs.modalSubmitBtn.textContent = "Login";
     }
+    modalToggle();
+}
+
+export function modalToggle() {
     refs.modal.classList.toggle("modal-hidden");
 }
 
-// функція реєстрації
-function registerSubmitHandler(event) {
-    // console.log("register");
-    event.preventDefault();
-    const form = event.currentTarget;
-    const login = form.elements.login.value;
-    const password = form.elements.password.value;
-
-    createUserWithEmailAndPassword(auth, login, password)
-        .then(userCredential => {
-            const user = userCredential.user;
-
-            set(ref(database, "users/" + user.uid), {
-                email: login,
-                registerDate: lastLoginDate.toLocaleString(),
-            })
-                .then(() => {
-                    Notiflix.Notify.success("Реєстрація успішна");
-                    form.reset();
-                    refs.modal.classList.toggle("modal-hidden");
-                    refs.modalForm.removeEventListener(
-                        "submit",
-                        registerSubmitHandler
-                    );
-                })
-                .catch(error => {
-                    Notiflix.Notify.failure(
-                        `Помилка реєстрації ${error.message}`
-                    );
-                });
-        })
-        .catch(error => {
-            const errorMessage = error.message;
-            Notiflix.Notify.failure(`Помилка реєстрації ${error.message}`);
-        });
-}
-
-// функція логіну
-function loginSubmitHandler(event) {
-    console.log("login");
-    console.log(auth);
-    event.preventDefault();
-    const form = event.currentTarget;
-    const login = form.elements.login.value;
-    const password = form.elements.password.value;
-
-    signInWithEmailAndPassword(auth, login, password)
-        .then(userCredential => {
-            const user = userCredential.user;
-            // додати або оновити властивість lastLoginDate в конкретного користувача
-            update(ref(database, "users/" + user.uid), {
-                lastLoginDate: lastLoginDate.toLocaleString(),
-            })
-                .then(() => {
-                    Notiflix.Notify.success("Авторизація успішна");
-                    form.reset();
-                    refs.modal.classList.toggle("modal-hidden");
-                    refs.modalForm.removeEventListener(
-                        "submit",
-                        loginSubmitHandler
-                    );
-                })
-                .catch(error => {
-                    Notiflix.Notify.failure(`Помилка входу ${error.message}`);
-                });
-        })
-        .catch(error => {
-            const errorMessage = error.message;
-            Notiflix.Notify.failure(`Помилка входу ${error.message}`);
-        });
-}
-
-function logOff() {
-    console.log("logoff");
-    console.log(auth);
-    signOut(auth)
-        .then(() => {
-            Notiflix.Notify.warning("Авторизацію скасовано");
-        })
-        .catch(error => {
-            Notiflix.Notify.warning(
-                `Виникли проблеми при виході: ${error.message}`
-            );
-        });
-}
-
-const rawData = [
-    { id: 0, name: "Yurii Soroka", number: "111-11-11" },
-    { id: 1, name: "Maria Soroka", number: "222-22-22" },
-    { id: 2, name: "Maxim Soroka", number: "333-33-33" },
-];
-
-function writeUserDataToDatabase(data) {
-    const userId = auth.currentUser.uid;
-    update(ref(database, "users/" + userId), {
-        queue: data,
-    });
+export function clearEventListeners() {
+    refs.modalForm.removeEventListener("submit", registerSubmitHandler);
+    refs.modalForm.removeEventListener("submit", loginSubmitHandler);
 }
